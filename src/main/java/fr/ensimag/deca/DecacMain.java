7d24c5e6e9381bda13a25259e8eedd905d875d8c
smaalim <Merieme.Smaali@grenoble-inp.org>
2025-12-16 23:45:33 +0100

    modification decacmain (fonction getparallel)

--- src/main/java/fr/ensimag/deca/DecacMain.java
+++ src/main/java/fr/ensimag/deca/DecacMain.java
@@ -6,6 +6,8 @@ import org.apache.log4j.Logger;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 
 /**
@@ -37,13 +39,39 @@ public class DecacMain {
             throw new UnsupportedOperationException("decac without argument not yet implemented");
         }
         if (options.getParallel()) {
-            // A FAIRE : instancier DecacCompiler pour chaque fichier à
+            //Fait au dessous: Instancier DecacCompiler pour chaque fichier à
             // compiler, et lancer l'exécution des méthodes compile() de chaque
             // instance en parallèle. Il est conseillé d'utiliser
             // java.util.concurrent de la bibliothèque standard Java.
+            int nbThreads = Runtime.getRuntime().availableProcessors();
+            ExecutorService executor = Executors.newFixedThreadPool(nbThreads);
+            List<Future<Boolean>> resultats = new ArrayList<>();
 
-            throw new UnsupportedOperationException("Parallel build not yet implemented");
-        } else {
+            for (File source : options.getSourceFiles()) {
+                DecacCompiler compiler = new DecacCompiler(options, source);
+
+                Future<Boolean> res = executor.submit(() -> {
+                    return compiler.compile();
+                });
+
+                resultats.add(res);
+            }
+
+            for (Future<Boolean> res : resultats) {
+                try {
+                    if (res.get()) {
+                        error = true;
+                    }
+                } catch (InterruptedException e) {
+                    error = true;
+                } catch (ExecutionException e) {
+                    error = true;
+                }
+            }
+
+            executor.shutdown();
+
+            } else {
             for (File source : options.getSourceFiles()) {
                 DecacCompiler compiler = new DecacCompiler(options, source);
                 if (compiler.compile()) {
